22d58b0191f558c04e068fe6a583c6d3881e33ae
informaticaesne <informatica@esne.es>
2024-05-16 21:38:50 +0200

    Cambios  con dataset sin imagenes

--- scripts/train_yolov8.py
+++ scripts/train_yolov8.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO("yolov8m.yaml")  # build a new model from scratch
+    model = YOLO("yolov9e.yaml")  # build a new model from scratch
     # Use the model
     model.train(data = r"E:\UDIT\SDC_dataset\dataset.yaml",cfg=r"E:\UDIT\UDIT_SDC\cfg\default.yaml")  # train the model
